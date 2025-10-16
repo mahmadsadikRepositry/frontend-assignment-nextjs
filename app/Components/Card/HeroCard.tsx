cadd03f8f4be52027cfd34a7ba5d8809ce76d4b9
sadikkoliya@gmail.com <sadikkoliya@gmail.com>
2025-10-15 21:57:01 -0400

    hercard

--- app/Components/Card/HeroCard.tsx
+++ app/Components/Card/HeroCard.tsx
@@ -4,8 +4,9 @@ export const HeroCard = ({ title, description, img }: { title: string; descripti
     return (
         <Card>
             {img && <img src={img} alt={title} className="rounded-t-lg w-full h-24 object-contain" />}
-            <div className="p-4">
-                <h3 className="text-lg font-semibold mb-2">{title}</h3>
+            <div className="p-4 text-center">
+                <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+                <hr className="my-2 border-blue-200" />
                 <p className="text-gray-600">{description}</p>
             </div>
         </Card>
